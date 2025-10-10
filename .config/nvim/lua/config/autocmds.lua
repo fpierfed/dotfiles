8832d3639104ce04e2dcf2a3730a737f245065a9
Francesco Pierfederici <fpierfed@gmail.com>
2025-10-10 16:32:18 +0200

    initial

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,17 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Do not start a new line with a comments if the current line is a comment.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "r", "o" })
+    vim.cmd("highlight ColorColumn ctermbg=0 guibg=#333333")
+  end,
+})
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Disable mouse mode
+vim.opt.mouse = ""
+vim.opt.mousescroll = "ver:0,hor:0"
+-- Do not center the cursor horizontally!
+vim.opt.sidescrolloff = 0
+vim.opt.sidescroll = 0
